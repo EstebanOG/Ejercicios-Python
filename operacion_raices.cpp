c7e7e286a346a7b1eac7e556d33613b73bff0b3c
juanesteban199911@gmail.com <juanesteban199911@gmail.com>
2020-02-25 15:48:19 -0500

    ejercicio python

---
+++ operacion_raices.cpp
@@ -0,0 +1,129 @@
+//#include "raiz.h"
+#include<iostream>
+#include<cmath>
+/*#pragma once*/
+
+using namespace std;
+
+void leerCoeficientes (float &a, float &b, float &c)
+{
+cout << "Ingrese los valores de a b y c de la ecuacion" << endl;
+cin >> a >> b >> c;
+
+}
+
+int discriminante (float a, float b, float c, float &disc, int &tiporaiz)
+{
+if (a != 0)
+{
+	disc = (b*b) - (4 * (a)*(c));
+	if (disc == 0)
+		tiporaiz = 0;
+	else
+	if (disc > 0)
+		tiporaiz = 1;
+	else tiporaiz = -1;
+}
+
+return tiporaiz;
+}
+void raicesIguales(float a, float b, float &x1,float &x2,float disc)
+{
+x1 = x2 = (-b*disc / (2 * a));
+
+cout << "los tipos de raices son iguales" << endl;
+cout << "El valor de la raiz es de = " << discriminante << endl;
+cout << "Los resultados de la ecuacion es" << endl;
+cout << "x1=" << x1 << endl;
+cout << "x2=" << x2 << endl;
+
+}
+
+void raicesNormales(float a, float b, float &x1, float &x2, float &disc  )
+{
+x1 = (((-1 * (b)) - sqrt(disc)) / 2 * a);
+x2 = (((-1 * (b)) + sqrt(disc)) / 2 * a);
+cout << "los tipos de raices son normales" << endl;
+cout << "El valor de la raiz es de = " << disc << endl;
+cout << "Los resultados de la ecuacion es" << endl;
+cout << "x1=  " << x1 << endl;
+cout << "x2 = " << x2 << endl;
+}
+
+void raicesImaginarias(float a, float b, float &x1, float &x2, float &disc ){
+x1 = (-b +(disc))/ (2 * a);
+x2 = (-b+-(disc))/ ( 2*a);
+
+cout << "los tipos de raices son imaginarias" << endl;
+cout << "El valor de la raiz es de = " << disc<< endl;
+
+cout << "Los resultados de la ecuacion es" << endl;
+cout << "x1=" << x1 << " + " << x2 << "i" << endl;
+cout << "x2=" << x1 << " - " << x2 << "i" << endl;
+
+}
+
+int main(void)
+{
+float a, b, c;
+
+
+float x1, x2, disc;
+int tiporaiz;
+leerCoeficientes(a, b, c);
+tiporaiz = discriminante(a, b, c, disc,tiporaiz);
+
+
+switch (tiporaiz)
+{
+case 0:
+raicesIguales(a, b, x1, x2,disc);
+break;
+case 1:
+raicesNormales(a, b,  x1, x2, disc);
+break;
+case -1:
+raicesImaginarias(a, b, x1, x2, disc);
+
+default:
+cout << "opcion invalida" << endl;
+
+}
+
+system("pause");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def leer_coeficinetes(a,b,c):
+    print("Ingrese los valores de a b y c de la ecuacion")
+    a = input("a: ")
+    b = input("b: ")
+    c = input("c: ")
+def discriminante():
+    if a != 0:
+        disc = (b*b) - (4*a*c)
+        if disc == 0:
+            tipo_raiz = 0
+        elif disc > 0:
+            tipo_raiz = 1
+        else:
+            tipo_raiz = -1
+    return tipo_raiz
+def raices_iguales()
+    x1 = x2 =
+leer_coeficinetes(a, b, c)
+tipo_raiz = discriminante(a, b, c, disc, tipo_raiz)
+#hacer switch
+
