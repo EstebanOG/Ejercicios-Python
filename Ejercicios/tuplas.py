ce8b6f89c3fcb9d6541e5b65e087463f2837698d
Esteban <juanesteban199911@gmail.com>
2020-05-25 11:51:56 -0500

    Ejercicios python

---
+++ Ejercicios/tuplas.py
@@ -0,0 +1,13 @@
+#Imprimir tupla con while
+print("Imprimir tupla con while")
+tupla = (21, 1.77, "Esteban")
+indice = 0
+while indice < len(tupla):
+    print(tupla[indice])
+    indice += 1
+
+#Imprimir tupla con for
+print("Imprimir tupla con for")
+tupla = (12,65,98,39,48) 
+for numeros in tupla:
+    print(numeros)
